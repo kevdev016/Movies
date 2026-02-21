7cc100a0504900674441522ca9c4346a58c2ad14
kevinjeggy <kevin.jeggy@strollby.com>
2026-02-21 15:42:31 +0530

    feat: [Listing] listing card initial setup

---
+++ Movies/Listing/View/MovieListingCard.swift
@@ -0,0 +1,15 @@
+//
+//  MovieListingCard.swift
+//  Movies
+//
+//  Created by Kevin Jeggy(UST, IN) on 21/02/26.
+//
+
+import SwiftUI
+
+struct MovieListingCard: View {
+    let movie: Movie
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
